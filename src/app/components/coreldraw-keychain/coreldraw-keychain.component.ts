59bfa4560b9f2919f22de6a3a71cf3577a6e2178
p-328 <ramaaryaanjali@gmail.com>
2022-05-13 08:20:27 -0400

    correction

--- src/app/components/coreldraw-keychain/coreldraw-keychain.component.ts
+++ src/app/components/coreldraw-keychain/coreldraw-keychain.component.ts
@@ -9,7 +9,7 @@ export class CoreldrawKeychainComponent implements OnInit {
   rules = [
     "In order to make a cut, you must select the outline of the line you want to cut to be red. \nAnything else will just be engraved.",
     "You can upload DXF/DWG files in your drawing, but any text you put in must be put on the area of the DXF/DWG file.",
-    "You can set a ratio in the height and length of a shape by selecting the lock icons on Width and Height."
+    "You can set a ratio in the height and width of a shape by selecting the lock icons on Width and Height."
   ]
   constructor() { }
 
